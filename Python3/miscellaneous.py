cb30cb783ac5304fdd69b981f8d5d4501f11b918
whokilleddb <debjeetbanerjee06042001@gmail.com>
2021-03-23 23:59:31 +0530

    Added Colors/Format Options In Python3/cmd.py

--- Python3/miscellaneous.py
+++ Python3/miscellaneous.py
@@ -5,3 +5,18 @@
 # 2. Dictionaries of Important hashes
 # 3. Move Media to /tmp
 # 4. More
+
+# Colours/Format Options For Terminal
+class fmt :
+    NONE='\033[00m'
+    BLACK='\033[01;30m'
+    RED='\033[01;31m'
+    GREEN='\033[01;32m'
+    YELLOW='\033[01;33m'
+    BLUE='\033[0;34m'
+    PURPLE='\033[01;35m'
+    CYAN='\033[01;36m'
+    WHITE='\033[01;37m'
+    BOLD='\033[1m'
+    BLINK='\033[5m'
+    UNDERLINE='\033[4m'
