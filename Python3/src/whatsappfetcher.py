aa16f9c732a09a7c45f7628910b088109cb9d5ba
whokilleddb <debjeetbanerjee06042001@gmail.com>
2021-03-25 00:25:51 +0530

    Added Selenium Class Skeleton

--- Python3/src/whatsappfetcher.py
+++ Python3/src/whatsappfetcher.py
@@ -1,3 +1,14 @@
 #!/bin/python3
 from miscellaneous import *
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+class whatsbot :
+    def __init__(self,browser,contact) :
+        self.browser=browser
+        self.contact=contact
