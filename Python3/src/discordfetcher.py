a777ccf10e331406007ee162cfbc96a9c242d0c0
hrithik20007 <darthvader20137@gmail.com>
2021-04-03 22:40:27 +0530

    Made the discordfetcher.py code more readable

--- Python3/src/discordfetcher.py
+++ Python3/src/discordfetcher.py
@@ -1,78 +1,40 @@
 #!/bin/python3
 
-# This file will contain the functions which include :
-# 1. Extracting message from discord and returning their strings
-# 2. Mapping unicode characters with certain commonly used commands like starting a listening port, executing a brute force attack
-# 3. Returning the Output with the original command
-# 4. Downloading images and gifs and calculating their md5sum followed by mapping them to specific commands like (2)
-# 5. More
-#
-# Note :
-# Call all Functions using threads (Use Low Level Threading Like : start_new_thread)
-# Store all downloaded media in /tmp       
-# Use Asyncio ?
-
-
 import discord
 import requests
-#import json
-#import random
 from discord.ext import commands
 import subprocess
-#import urllib
-#from decouple import config
+from dotenv import load_dotenv
 import os
-#import youtube_dl
-#import time
 import asyncio
 
-'''
-#try add this 
-intents=discord.Intents.all()
-#if the above don't work, try with this
-#intents = discord.Intents.default()
-#intents.members=True
+load_dotenv()  #Makes the .env file accessible to my script as the source of the environment variables
 
-client=discord.Client(intents=intents)
-'''
-
-ID=int(824362867657801779)
-
+ID=int(os.getenv("ID"))
 bot=commands.Bot(command_prefix="!")
 
-#To download a sent image
-def download(url,path):
+def download(url,path):  #To download a sent image
 
-    #We get a response object from the URL. This object further has attributes which help us navigate the different functionalities of the page.
-    r=requests.get(url)
-
-    #r.content contains the bytes of the image, so when we write that into a file, it is interpreted as an image
-    #'wb' mode stands for write-bytes
-    with open(path,'wb') as f:
+    r=requests.get(url)  #We get a response object from the URL. This object further has attributes which help us navigate the different functionalities of the page.
+    with open(path,'wb') as f:  #r.content contains the bytes of the image, so when we write that into a file, it is interpreted as an image ('wb' mode stands for write-bytes)
         f.write(r.content)
 
-#To calculate the md5sum of a sent image
-def md5sum(a):
+def md5sum(a):  #To calculate the md5sum of a sent image
+
     args=f"md5sum {a}"
-    #Shell=True means the args command is executed by using a shell. The stdout(output) and stderr(error) pipes are combined as per subprocess documentation
-    process= subprocess.Popen(args,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    process= subprocess.Popen(args,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)  #Shell=True means the args command is executed by using a shell. The stdout(output) and stderr(error) pipes are combined as per subprocess documentation
     output= process.stdout.read().decode()
     s=output.split(" ")
     return s[0]
     
-
-#Bot activation message while logging in
-@bot.event
+@bot.event  #Bot activation message while logging in
 async def on_ready():
      print("You have logged in as {0}".format(bot.user))
      channel=bot.get_channel(ID)
      await channel.send("Welcome back Mr.Anderson!")
+     await bot.process_commands()  #We cannot trigger the bot.command instructions wihout writing this line in case of bot.event
 
-    #We cannot trigger the bot.command instructions wihout writing this line in case of bot.event
-     await bot.process_commands()
-
-#A casual hello command for returning a pre-defined message
-@bot.event
+@bot.event  #For commands
 async def on_message(message):
     if message.author==bot.user:
         return
@@ -80,16 +42,14 @@ async def on_message(message):
         if message.content.startswith("!hello"):
             await message.channel.send("Welcome to the matrix.")
 
-        #Calculating the md5sum of any image sent to the server
-        if message.content.startswith("!attach"):
+        if message.content.startswith("!attach"):  #Calculating the md5sum of any image sent to the server
             attachments=message.attachments
             attach=attachments[0].url
             await message.channel.send(attach)
 
             your_path='/home/vader/Downloads/'
             name='fileee'
-            #The message.attachment object url has some some special characters at the end which the browser can't seem to parse (like %27%). So we remove that portion
-            url=attach.split('%')[0]
+            url=attach.split('%')[0]  #The message.attachment object url has some some special characters at the end which the browser can't seem to parse (like %27%). So we remove that portion
             path=str(your_path+name)
             download(url,path)
         
@@ -97,21 +57,10 @@ async def on_message(message):
             await message.channel.send(output2)
 
             os.remove(path)
-        '''     
-            if output2 in dictionary.keys():
-                command=str(dictionary[output2])
-                process2= subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
-                output3= process2.stdout.read().decode()
-                
-                await message.channel.send(output3)
-            else:
-                await message.channel.send("The md5sum is not present in the dictionary.")
-        '''
-    #We cannot trigger the bot commands wihout writing this line in case of bot.event
-    await bot.process_commands(message)
 
-#Taking any number of words as arguements and printing them
-@bot.command()
+    await bot.process_commands(message)  #We cannot trigger the bot commands wihout writing this line in case of bot.event
+
+@bot.command()  #Taking any number of words as arguements and printing them
 async def print(ctx, *args):
     output=""
     for arg in args:
@@ -119,4 +68,4 @@ async def print(ctx, *args):
     await ctx.channel.send(output)
     
 
-bot.run("TOKEN")
+bot.run(os.getenv("TOKEN"))
